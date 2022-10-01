9e5ae6cb1b2d218ebc9ab1ee707e04885eabd5be
CayCYS <CesarCayCYS@gmail.com>
2022-10-01 18:06:17 -0500

    Primer Coomit

---
+++ Primer - Nivel/Reto 2 - Hola… nombre y apellido/Hola Nombre y Apellido.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+int main (){
+    char nom[20];
+    printf("Hello Word My names is Tomas, What is your name? \n");
+    scanf("%s",&nom);
+    printf("My name is %s",nom);
+    return 0;
+}
