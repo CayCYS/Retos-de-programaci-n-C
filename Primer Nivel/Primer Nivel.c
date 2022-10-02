4b0fc9750daa597ab4808f601d8557c3676abed3
CayCYS <CesarCayCYS@gmail.com>
2022-10-01 20:30:48 -0500

    Reto 7 Primer Nivel

--- Primer Nivel/Primer Nivel.c
+++ Primer Nivel/Primer Nivel.c
@@ -11,7 +11,7 @@ int main(){
     
     //Reto 3 Mensaje multilínea
     printf("\nReto 3 \n");
-    printf("Platzi cuenta con cursos de: \nIngles\nMatematicas \nProgramacion \nLiderazgos");
+    printf("Platzi cuenta con cursos de: \nIngles\nMatematicas \nProgramacion \nLiderazgos\n");
     
     //Reto 4 Sumador
     printf("\nReto 4 \n");
@@ -21,7 +21,7 @@ int main(){
     printf("Ingrese el segundo digito: ");
     scanf("%f",&y);
     z=x+y;
-    printf("Suma es %f + %f = %.3f",x,y,z);
+    printf("Suma es %f + %f = %.3f\n",x,y,z);
     
     //Reto 5 Suma y multiplicacion
     printf("\nReto 5 \n");
@@ -34,6 +34,28 @@ int main(){
     scanf("%d",&z1);
     w1=(x1+y1)*z1 ;
     printf("Valores son: %d, %d, %d \n",x1,y1,z1);
-    printf("Resultado es: %d",w1);
+    printf("Resultado es: %d",w1\n);
+
+    //Reto 6 Rebanadas de Pizza
+    printf("\nReto 6 \n");
+    int rebanada,consumo,pizza;
+    printf("Ingrese el primer digito: ");
+    scanf("%d",&rebanada);
+    printf("Ingrese el segundo digito: ");
+    scanf("%d",&consumo);
+    pizza = rebanada - consumo;
+    printf("Sobra un numero de Rebanadas: %d",pizza\n);
+
+    //Reto 7 Rebanadas de Pizza
+    printf("\nReto 7 \n");
+    int tatal,personas,propina,iva;
+    printf("El total a pagar:");
+    scanf("%d",&total);
+    printf("Personas:");
+    scanf("%d",&personas);
+    printf("Porcentajes de propina");
+    scanf("%d",&propina);
+    printf("Porcentajes iva");
+    scanf("%d",&iva);
     return 0;
 }
